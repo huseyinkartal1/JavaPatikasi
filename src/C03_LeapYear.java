5fc377a4b670d51dd187f0c0c88918d7c88d1518
archer1903 <98432117+archer1903@users.noreply.github.com>
2022-04-10 13:28:23 +0300

    C03_LeapYear

---
+++ src/C03_LeapYear.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class C03_LeapYear {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please write a year: ");
+        int year = scanner.nextInt();
+
+        if(year%100==0){
+            if(year%400==0) {
+                System.out.println("This is a leap year.");
+            }else {
+            System.err.println("This is NOT a leap year.");
+        }
+        }else if(year%4==0){
+            System.out.println("This is a leap year.");
+        }else {
+            System.err.println("This is NOT a leap year.");
+        }
+
+
+    }
+}
